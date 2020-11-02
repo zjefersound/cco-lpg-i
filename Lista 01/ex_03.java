6f5ac637679fb7965ce75d4fe30c0c6a6cef5ca8
zjefersound <jeferson.2017317264@gmail.com>
2020-11-02 19:00:26 -0300

    ex 4

--- Lista 01/ex_03.java
+++ Lista 01/ex_03.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
 
 public class ex_03 {
-  /**
-   * ax*x + bx + c
-   * Delta = b**2 -4 * a * c
-   * (-b +- sqrt(Delta))/2a
-   */
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     double salario, salarioFinal, imposto, gratificacao;
@@ -15,7 +10,7 @@ public class ex_03 {
     gratificacao = 5;
     
     System.out.println("Digite o seu salário:");
-    salario = keyboard.nextFloat();
+    salario = keyboard.nextDouble();
     salarioFinal = (100+gratificacao-imposto)/100*salario;
 
     System.out.println("Salario Final:"+salarioFinal);
---
+++ Lista 01/ex_04.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ex_04 {
+  /**
+    o valor de cada quilowatt;
+    o valor a ser pago por essa residência;
+    o valo a ser pago com 15% de desconto
+   */
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    float salarioMinimo, quantidadeKw, kw, custoResidencia, desconto, custoResidenciaComDesconto;
+
+    //porcentagem
+    desconto = 15;
+    
+    System.out.println("Digite o seu salário:");
+    salarioMinimo = keyboard.nextFloat();
+
+    System.out.println("Digite a quantidade de Quilowatts:");
+    quantidadeKw = keyboard.nextFloat();
+
+    kw = salarioMinimo / 5;
+    custoResidencia = quantidadeKw * kw;
+
+    custoResidenciaComDesconto = (100-desconto)/100*custoResidencia;
+
+    System.out.println("Valow KW:"+kw);
+    System.out.println("Custo da Residencia:"+custoResidencia);
+    System.out.println("Custo da Residencia com "+desconto+"%:"+custoResidenciaComDesconto);
+  }
+}
