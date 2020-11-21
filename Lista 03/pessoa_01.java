cc51f4cc642a6d53016f55a35ad0a57bce8d2d89
zjefersound <jeferson.2017317264@gmail.com>
2020-11-20 21:09:58 -0300

    Classe pessoa

--- Lista 03/pessoa_01.java
+++ Lista 03/pessoa_01.java
@@ -4,9 +4,91 @@ public class pessoa_01 {
   /**
    * Pessoa loja varejista
    */
+
+  public static void ColetarDadosCadastro(Pessoa pessoa) {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Comece criando um usuario:");
+
+    System.out.println("Digite o cpf:");
+    String cpf = keyboard.nextLine();
+
+    System.out.println("Digite o nome:");
+    String nome = keyboard.nextLine();
+
+    System.out.println("Digite o telefone:");
+    String telefone = keyboard.nextLine();
+
+    System.out.println("Digite o email:");
+    String email = keyboard.nextLine();
+
+    System.out.println("Digite o endereco:");
+    String endereco = keyboard.nextLine();
+
+    pessoa.CadastrarPessoa(cpf, nome, telefone, email, endereco);
+  }
+  
+  public static void ColetarDadosAlteracao(Pessoa pessoa) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Digite os dados que quer alterar");
+    System.out.println("Caso nao queira aperte Enter");
+
+    System.out.println("Digite o cpf:");
+    String cpf = keyboard.nextLine();
+
+    System.out.println("Digite o nome:");
+    String nome = keyboard.nextLine();
+
+    System.out.println("Digite o telefone:");
+    String telefone = keyboard.nextLine();
+
+    System.out.println("Digite o email:");
+    String email = keyboard.nextLine();
+
+    System.out.println("Digite o endereco:");
+    String endereco = keyboard.nextLine();
+
+    pessoa.AlterarPessoa(cpf, nome, telefone, email, endereco);
+  }
+  
+  public static void ColetarDadosExclusao(Pessoa pessoa) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Digite o cpf:");
+    String cpf = keyboard.nextLine();
+
+    pessoa.DeletarPessoa(cpf);
+  }
+
+  public static void listaOpcoes() {
+    System.out.println("Escolha uma acao");
+    System.out.println("0. Sair");
+    System.out.println("1. Editar");
+    System.out.println("2. Mostrar");
+    System.out.println("3. Excluir");
+  }
+
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
-    
+    int inputOptionNumber = 0;
+    Pessoa pessoa = new Pessoa();
+    ColetarDadosCadastro(pessoa);
+    do {
+      listaOpcoes();
+      inputOptionNumber = keyboard.nextInt();
+      switch (inputOptionNumber) {
+        case 1:
+          ColetarDadosAlteracao(pessoa);
+          break;
+        case 2:
+          pessoa.MostrarPessoa();
+          break;
+        case 3:
+          ColetarDadosExclusao(pessoa);
+          break;
+      }
+    } while (inputOptionNumber != 0);
+    System.out.println("Sistema encerrado");
+
     keyboard.close();
   }
 }
@@ -22,15 +104,15 @@ class Pessoa {
 
   }
 
-  public MostrarPessoa() {
-    System.out.println("cpf: "+this.cpf);
-    System.out.println("nome: "+this.nome);
-    System.out.println("telefone: "+this.telefone);
-    System.out.println("email: "+this.email);
-    System.out.println("endereco: "+this.endereco);
+  public void MostrarPessoa() {
+    System.out.println("cpf: " + this.cpf);
+    System.out.println("nome: " + this.nome);
+    System.out.println("telefone: " + this.telefone);
+    System.out.println("email: " + this.email);
+    System.out.println("endereco: " + this.endereco);
   }
 
-  public CadastrarPessoa(String cpf, String nome, String telefone, String email, String endereco) {
+  public void CadastrarPessoa(String cpf, String nome, String telefone, String email, String endereco) {
     this.cpf = cpf;
     this.nome = nome;
     this.telefone = telefone;
@@ -39,18 +121,25 @@ class Pessoa {
     System.out.println("Criou");
   }
 
-  public AlterarPessoa(String cpf, String nome, String telefone, String email, String endereco) {
-    this.cpf = cpf;
-    this.nome = nome;
-    this.telefone = telefone;
-    this.email = email;
-    this.endereco = endereco;
+  public void AlterarPessoa(String cpf, String nome, String telefone, String email, String endereco) {
+    this.cpf = !cpf.isEmpty() ? cpf : this.cpf;
+    this.nome = !nome.isEmpty() ? nome : this.nome;
+    this.telefone = !telefone.isEmpty() ? telefone : this.telefone;
+    this.email = !email.isEmpty() ? email : this.email;
+    this.endereco = !endereco.isEmpty() ? endereco : this.endereco;
     System.out.println("Alterou");
   }
-  
-  public DeletarPessoa(String cpf) {
-    if (this.cpf == cpf) {
+
+  public void DeletarPessoa(String cpf) {
+    if (cpf.equalsIgnoreCase(this.cpf)) {
+      this.cpf = null;
+      this.nome = null;
+      this.telefone = null;
+      this.email = null;
+      this.endereco = null;
       System.out.println("Deletou");
+    } else {
+      System.out.println("CPF invalido");
     }
   }
 };
